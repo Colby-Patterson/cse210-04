e678c5f6485d5db5d2308452f6c067944b8f201a
Kovekk <fishm3001@gmail.com>
2022-02-24 20:31:46 -0700

    Starting the game and making the character

--- greed/__main__.py
+++ greed/__main__.py
@@ -3,6 +3,7 @@ import random
 
 from game.casting.actor import Actor
 from game.casting.artifact import Artifact
+from game.casting.character import Character
 from game.casting.cast import Cast
 
 from game.directing.director import Director
@@ -24,7 +25,36 @@ CAPTION = "Greed"
 WHITE = Color(255, 255, 255)
 
 def main():
-    pass
+    """satisfy python"""
+    #create cast
+    cast = Cast()
+
+    #create player character
+    x = int(MAX_X / 2)
+    y = 0
+    start_position = Point(x, y)
+
+    character = Character()
+    character.set_text('#')
+    character.set_size(FONT_SIZE)
+    character.set_axis(1)
+    character.set_position(start_position)
+    character.set_color(WHITE)
+
+    cast.add_actor('character', character)
+
+    #create banner
+    banner = Actor()
+    banner.set_text('Score: 0')
+    banner.set_size(FONT_SIZE)
+    banner.set_color(WHITE)
+    banner.set_position(Point(CELL_SIZE, 0))
+
+    #start the game
+    keyboard_service = KeyboardService(CELL_SIZE)
+    video_service = VideoService(CAPTION, MAX_X, MAX_Y, CELL_SIZE, FRAME_RATE)
+    director = Director(keyboard_service, video_service)
+    director.start_game(cast)
 
 if __name__ == "__main__":
     main()
---
+++ greed/game/casting/character.py
@@ -0,0 +1,17 @@
+from game.casting.actor import Actor
+
+class Character(Actor):
+    """tracks the characters score."""
+
+    def __init__(self):
+        super.__init__()
+        self._score = 0
+
+    def get_score(self):
+        """returns the score as an Int."""
+        return self._score
+
+    def set_score(self, score):
+        """sets the characters score.
+            score (Int)"""
+        self._score = score
