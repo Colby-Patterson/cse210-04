765d2454b95794654d2021772dd5252bdb92778d
Ethan Chipman <ethandchipman@gmail.com>
2022-02-24 20:06:59 -0700

    For you

--- greed/__main__.py
+++ greed/__main__.py
@@ -13,6 +13,16 @@ from game.services.video_service import VideoService
 from game.shared.color import Color
 from game.shared.point import Point
 
+FRAME_RATE = 12
+MAX_X = 900
+MAX_Y = 600
+CELL_SIZE = 15
+FONT_SIZE = 15
+COLS = 60
+ROWS = 40
+CAPTION = "Greed"
+WHITE = Color(255, 255, 255)
+
 def main():
     pass
 
