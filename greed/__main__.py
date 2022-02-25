e939fa938ccd0183a878c570aa19c5eac993b83e
Colby Patterson <cpatt@byui.edu>
2022-02-24 19:30:13 -0700

    Added imports to main

--- greed/__main__.py
+++ greed/__main__.py
@@ -1,5 +1,20 @@
+import os
+import random
+
+from game.casting.actor import Actor
+#from game.casting.artifact import Artifact
+#from game.casting.cast import Cast
+
+from game.directing.director import Director
+
+from game.services.keyboard_service import KeyboardService
+from game.services.video_service import VideoService
+
+from game.shared.color import Color
+from game.shared.point import Point
+
 def main():
     pass
 
-if __name__ == "__main.py__":
+if __name__ == "__main__":
     main()
